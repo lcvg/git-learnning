98faa2b964454dd03c465440a2c584edfda9ac37
黄重杨 <huangcy@yunrong.cn>
2019-02-21 20:23:32 +0800

    rebase test3

--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@ update readmerebase test again
 rebase test again
 
 merge test
-
+dev update rebase to dev2
 hell;
 
--- rebase.d
+++ rebase.d
@@ -3,3 +3,4 @@ hello
 dev update rebase to temp
 dev update rebase to dev
 dev update rebase to dev2
+dev update rebase to dev23
